d6906311d2890a6bf7ec9cba638b3fd6c97feceb
Selan R dos Santos <selan.santos@ufrn.br>
2023-02-18 21:58:29 -0300

    new symbol for workspace 7

--- config.def.h
+++ config.def.h
@@ -72,7 +72,7 @@ static Sp scratchpads[] = {
 
 /* tagging */
 // static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *tags[] = { " ", " ", " ", "ﭮ ", " ", " ", " ", " ", " "};
+static const char *tags[] = { " ", " ", " ", "ﭮ ", " ", " ", " ", " ", " "};
 
 static const Rule rules[] = {
 	/* xprop(1):
