e8a9a7da9fae5a1d064e6ba354c8a985861f08df
Selan R dos Santos <selan.santos@ufrn.br>
2023-06-12 10:33:31 -0300

    update icons

--- config.def.h
+++ config.def.h
@@ -74,8 +74,9 @@ static Sp scratchpads[] = {
 
 /* tagging */
 // static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *tags[] = { "", "", "", "󰙯", "", "", "", "", ""};
-
+// static const char *tags[] = { "", "", "", "󰙯", "", "", "", "", ""};
+static const char *tags[] = { "", "", "", "", "", "", "", "", ""};
+// 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
@@ -96,7 +97,8 @@ static const Rule rules[] = {
 	{ NULL,                 NULL,        "Brave",        1 << 1,       1,           0,           -1 },
 	{ "Skype",              NULL,        NULL,           1 << 5,       1,           1,           -1 },
 	{ "firefox",            NULL,        NULL,           1 << 1,       1,           0,           -1 },
-	{ "telegram-desktop",   NULL,        NULL,           1 << 2,       1,           0,           -1 },
+	{ "TelegramDesktop",    NULL,        NULL,           1 << 2,       1,           0,           -1 },
+	{ "Whatsapp-for-linux", NULL,        NULL,           1 << 2,       1,           0,           -1 },
 	{ "discord",            NULL,        NULL,           1 << 3,       1,           0,           -1 },
 	{ "VirtualBox Manager", NULL,        NULL,           1 << 7,       1,           1,           -1 },
 	{ "qBittorrent",        NULL,        NULL,           1 << 7,       1,           0,           -1 },
@@ -125,6 +127,13 @@ static const Layout layouts[] = {
 	{ NULL,       NULL },
 };
 
+	// { "[]=",      tile },    /* first entry is default */
+	// { "><>",      NULL },    /* no layout function means floating behavior */
+	// { "[M]",      monocle },
+	// { "|M|",      centeredmaster },
+	// { ">M>",      centeredfloatingmaster },
+	// { "TTT",      bstack },
+	// { "===",      bstackhoriz },
 /* key definitions */
 #define MODKEY Mod4Mask
 #define ALTKEY Mod1Mask
